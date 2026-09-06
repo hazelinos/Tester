eaee2bd4d0ddc0df92bab62c7f97c577e92d0eac
hazelinos <feezet1@gmail.com>
2026-09-06 13:59:59 +0700

    fix: remove broken dashboard stylesheet import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import './dashboard-reference.css'
 
 import { FinanceProvider } from './context/FinanceContext'
 import { SettingsProvider } from './context/SettingsContext'
